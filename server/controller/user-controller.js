ed74653efb7b6a37adae99d39ab18e4692fefb1b
adityar22 <adityaramadhanarckarito47@gmail.com>
2022-10-08 13:52:51 +0700

    login page

---
+++ server/controller/user-controller.js
@@ -0,0 +1,14 @@
+const { response } = require('express');
+var userDB = require('../model/user-model');
+
+exports.createUser=(req, res)=>{
+        
+}
+
+exports.authUser=(req, res)=>{
+
+}
+
+exports.editUser=(req,res)=>{
+    
+}
