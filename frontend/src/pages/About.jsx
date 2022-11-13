8b3991689760431b8086310ddc9c962e68bedfe5
fachrizalbayunugroho <fachri.faban@gmail.com>
2022-11-13 19:08:45 +0700

    edit

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -2,7 +2,7 @@ const About = () => {
     return (
         <div className="p-7 h-screen">
             <h1 className="mx-auto mt-12 text-2xl font-bold">About</h1>
-            <p>Lorem ipsum</p>
+            <p>Aplikasi ini merupakan aplikasi berbasis web yang membantu pengguna dalam mengelola task berdasarkan skala prioritas dan deadline</p>
         </div>
     );
 }
