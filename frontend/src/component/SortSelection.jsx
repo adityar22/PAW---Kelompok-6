e3482bd3e5a4c1d09df357544112f0988e4f8e9a
adityar22 <adityaramadhanarckarito47@gmail.com>
2022-11-14 14:28:25 +0700

    sync page-task

--- frontend/src/component/SortSelection.jsx
+++ frontend/src/component/SortSelection.jsx
@@ -3,11 +3,11 @@ const SortSelection = () => {
         <div className='flex'>
             <p className='px-2 mx-1 text-sm'>Sort By:</p>
             <select className='px-1 mx-1'>
-
+                <option></option>
             </select>
-            <p>Upp</p>
+            <p>Up</p>
         </div>
     );
 }
- 
+
 export default SortSelection;
--- frontend/src/component/TaskList.jsx
+++ frontend/src/component/TaskList.jsx
@@ -8,7 +8,7 @@ const TaskList = ({ task }) => {
             <td className="py-3 bg-white-800 p-3 text-sm font-semibold tracking-wide">{task.taskPriority}</td>
             <td className="py-3 bg-white-800 p-3 text-sm font-semibold tracking-wide">{task.taskTime}</td>
             <td className="py-3 bg-white-800 p-3 text-sm font-semibold tracking-wide">
-
+                
             </td>
         </tr>
     );
