f7522f14b292696f8986d1b72b83733923ef85a5
adityar22 <adityaramadhanarckarito47@gmail.com>
2022-11-17 20:15:13 +0700

    add edit task

--- frontend/src/pages/Calendar.jsx
+++ frontend/src/pages/Calendar.jsx
@@ -1,29 +1,49 @@
-import React from 'react'
-import FullCalendar from '@fullcalendar/react'
-import dayGridPlugin from '@fullcalendar/daygrid'
-import timeGridPlugin from '@fullcalendar/timegrid'
-import interactionGridPlugin from '@fullcalendar/interaction'
+import React, { useContext } from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionGridPlugin from "@fullcalendar/interaction";
+import { useState, useEffect, useRef } from "react";
+import { useTasksContext } from "../hooks/useTasksContext";
+import useFetch from "../hooks/useFetch";
+// import {useTasksContext} from "../context/TasksContext";
+// import AppState from "../contexts/CalContext";
 
 const Calendar = () => {
-    return ( <
-        div className = "py-10 px-28 h-screen" >
-        <
-        FullCalendar plugins = {
-            [dayGridPlugin, timeGridPlugin, interactionGridPlugin]
-        }
-        initialView = 'dayGridMonth'
-        headerToolbar = {
-            {
-                left: 'prev, next, today',
-                center: 'title',
-                right: 'dayGridMonth, timeGridWeek, timeGridDay',
-            }
-        }
-        height = { 700 }
-        contentHeight = { 700 }
-        />      <
-        /div>
+    //   const CalContext = useContext();
+    //   const { tasks, getTasks} = AppState
+    //   useEffect(() => {
+    //     getTasks()
+    //   }, [tasks])
+    const { tasks, dispatch, isPending, error, setLoading, setError } =
+        useTasksContext();
+    const [popup, setPopup] = useState(false);
+    const url = "/api/tasks";
+
+    const togglePopup = () => {
+        setPopup(!popup);
+    };
+
+    useFetch({ url, dispatch, setError, setLoading, type: "GET_TASKS" });
+
+    return (
+        <div className="py-10 px-28 h-screen">
+            <FullCalendar
+                plugins={[dayGridPlugin, timeGridPlugin, interactionGridPlugin]}
+                initialView="dayGridMonth"
+                headerToolbar={{
+                    left: "prev next today",
+                    center: "title",
+                    right: "dayGridMonth timeGridWeek timeGridDay",
+                }}
+                height={700}
+                contentHeight={700}
+                tasks={tasks}
+            //{tasks && tasks.map(task => (<TaskList key={task._id} task={task} />))}
+            // tasks={tasks}
+            />{" "}
+        </div>
     );
-}
+};
 
 export default Calendar;
