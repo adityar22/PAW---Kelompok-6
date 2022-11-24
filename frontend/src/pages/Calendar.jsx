796a839814677db29f5960edf5e29d96df1e4e93
adityar22 <adityaramadhanarckarito47@gmail.com>
2022-11-24 19:46:29 +0700

    not found

--- frontend/src/pages/Calendar.jsx
+++ frontend/src/pages/Calendar.jsx
@@ -1,11 +1,7 @@
-import React, { useState, useRef} from "react";
+import React, { useState, useRef } from "react";
 import { useEventContext } from "../hooks/useEventContext";
 import useFetch from "../hooks/useFetch";
 import { useDisplayContext } from "../hooks/useDisplayContext";
-
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import moment from 'moment';
 
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
@@ -28,35 +24,34 @@ const Calendar = () => {
     const togglePopup = () => {
         setPopup(!popup);
     };
-return (
-    <>
-        {popup && <AddEvent togglePopup={togglePopup} setLoading={setLoading} url={url} notify={notify} />}
-        <div className="py-10 px-28 w-full" >
-            <div className="text-4xl font-bold text-orange my-12 mx-auto">
-                <h1 className='text-5xl font-bold mb-12 text-dark-blue' >Add your events here! 📅</h1>
+    return (
+        <>
+            {popup && <AddEvent togglePopup={togglePopup} setLoading={setLoading} url={url} notify={notify} />}
+            <div className="py-10 px-28 w-full" >
+                <div className="text-4xl font-bold text-orange my-12 mx-auto">
+                    <h1 className='text-5xl font-bold mb-12 text-dark-blue' >Add your events here! 📅</h1>
+                </div>
+                <div className="align-middle mb-6">
+                    <button type="button" className="button mb-3" onClick={togglePopup}>Add Event +</button>
+                </div>
+                {error && <div className='font-semibold text-lg text-red-400 my-4'>Somehing error is occured 🙀</div>}
+                {isPending && <Loading />}
+                <FullCalendar
+                    plugins={
+                        [dayGridPlugin, timeGridPlugin, interactionGridPlugin]}
+                    initialView="dayGridMonth"
+                    headerToolbar={{
+                        left: "prev next today",
+                        center: "title",
+                        right: "dayGridMonth timeGridWeek timeGridDay",
+                    }}
+                    height={700}
+                    contentHeight={700}
+                    events={events}
+                />{" "}
             </div>
-            <div className="align-middle mb-6">
-                <button type="button" className="button mb-3" onClick={togglePopup}>Add Event +</button>
-            </div>
-            {error && <div className='font-semibold text-lg text-red-400 my-4'>Somehing error is occured 🙀</div>}
-            {isPending && <Loading />}
-            <FullCalendar
-                ref={calRef}
-                plugins={
-                    [dayGridPlugin, timeGridPlugin, interactionGridPlugin]}
-                initialView="dayGridMonth"
-                headerToolbar={{
-                    left: "prev next today",
-                    center: "title",
-                    right: "dayGridMonth timeGridWeek timeGridDay",
-                }}
-                height={700}
-                contentHeight={700}
-                events={events}
-            />{" "}
-        </div>
-    </>
-);
+        </>
+    );
 };
 
 export default Calendar;
