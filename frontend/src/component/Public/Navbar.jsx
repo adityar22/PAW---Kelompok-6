0de2fcff534dcc9b90acff4e0ddaf46fe610cad4
adityar22 <adityaramadhanarckarito47@gmail.com>
2022-11-25 02:12:06 +0700

    responsive navbar

--- frontend/src/component/Public/Navbar.jsx
+++ frontend/src/component/Public/Navbar.jsx
@@ -13,10 +13,12 @@ import menu_notes from "../../asset/menu_notes.png";
 import menu_task from "../../asset/menu_task.png";
 import logo_tman from "../../asset/logoTman.png";
 
+import "../../index.css"
+
 
 const Navbar = () => {
     const [open, setOpen] = useState(true);
-    const active = "bg-white text-orange"
+    const active = "bg-white text-orange font-bold"
     const deactive = "text-gray-300"
 
     const { logout } = useLogout();
@@ -37,60 +39,62 @@ const Navbar = () => {
     };
 
     return (
-        <div className={` ${open ? "w-72" : "w-20"} 
-            bg-dark-blue h-full p-5  pt-8  duration-300 relative`}>
-            <img src={controller} className={`absolute cursor-pointer -right-3 top-9 w-6 scale-150 rounded-full rotate-180  ${!open && "rotate-0"}`}
-                onClick={() => setOpen(!open)} alt=""
-            />
-            <div className="flex justify-between flex-col h-full">
-                <div className="content-top">
-                    <div className="flex gap-x-4 justify-center">
-                        <NavLink to="/">
-                            <img src={logo_tman} className={`w-12 inline-block mr-2 ${!open && "m-3"}`} />
-                            <h1
-                                className={`text-orange inline-block mb-12 cursor-pointer origin-left font-bold text-2xl duration-300  ${!open && "scale-0"}`}>
-                                T'Man
-                            </h1>
-                        </NavLink>
-                    </div>
-                    <ul className="pt-6">
-                        {Menus.map((menu, index) => (
-                            <div>
-                                <NavLink key={index} to={menu.link} className={({ isActive }) => isActive ? active : deactive}>
-                                    <div>
-                                        {menu.gap && <div className="inset-0 flex items-center px-2 mt-4"><div className="w-full border-b border-white border-opacity-30"></div></div>}
-                                        <li
-                                            className={` flex gap-3 rounded-md p-2 cursor-pointer hover:bg-blue-500 text-sm items-center transition-all duration-300 gap-x-4 
+        <>
+            <div className={`${open ? "w-full sm:w-72" : "w-full sm:w-20 h-20 sm:h-screen"} 
+            bg-dark-blue p-5  sm:pt-8  duration-300 fixed sm:relative z-40`}>
+                <img src={controller} className={`absolute cursor-pointer -right-0 mr-6 sm:mr-0 sm:-right-3 top-9 w-6 scale-150 rounded-full  ${!open ? "-rotate-90 sm:rotate-0" : "rotate-90 sm:rotate-180"}`}
+                    onClick={() => setOpen(!open)} alt=""
+                />
+                <div className="flex justify-between flex-col h-full ">
+                    <div className="content-top">
+                        <div className="flex gap-x-4 justify-start sm:justify-center">
+                            <NavLink to="/">
+                                <img src={logo_tman} className={`w-12 inline-block mr-2 ${!open && "sm:m-3"}`} />
+                                <h1
+                                    className={`text-orange inline-block mb-12 cursor-pointer origin-left font-bold text-2xl duration-300  ${!open && "sm:scale-0"}`}>
+                                    T'Man
+                                </h1>
+                            </NavLink>
+                        </div>
+                        <div className={!open ? "invisible sm:visible sm:pt-6" : "visible pt3 sm:pt-6"}>
+                            {Menus.map((menu, index) => (
+                                <div>
+                                    <NavLink key={index} to={menu.link} className={({ isActive }) => isActive ? active : deactive}>
+                                        <div>
+                                            {menu.gap && <div className="inset-0 flex items-center px-2 mt-4"><div className="w-full border-b border-white border-opacity-30"></div></div>}
+                                            <li
+                                                className={` flex gap-3 rounded-md p-2 cursor-pointer hover:bg-blue-500 text-sm items-center transition-all duration-300 gap-x-4 
                                 ${menu.gap ? "mt-4" : "mt-2"} 
                                 ${index === 0 && "bg-light-white"} `}
-                                        >
-                                            <img src={menu.src} className="w-4 h-4" alt="" />
-                                            <span className={`${!open && "hidden"} origin-left duration-200 text-base`}>
-                                                {menu.title}
-                                            </span>
-                                        </li>
-                                    </div>
-                                </NavLink>
-                            </div>
-                        ))}
-                    </ul>
-                </div>
-                <div className="content-bottom">
-                    <ul className={`flex flex-col items-center mb-3 text-gray-300 transition-all text-sm ${!open && "scale-0"}`}>
-                        <p >Logged as <span className="font-semibold text-orange">{user.username ? user.username : 'anonim'}</span></p>
-                        <p className="font-semibold text-orange">{user.email}</p>
-                    </ul>
-                    <ul>
-                        {user && (
-                            <div className="logout">
-                                {/* <span>{user.email}</span> */}
-                                {/* <button onClick={handleClick} className={`text-white bg-red-500 hover:bg-red-700 focus:scale-90 focus:transition-all rounded-lg px-5 py-2.5 text-center mb-2 w-full transition-all ${!open && "scale-0"}`}>Log out</button> */}
-                            </div>
-                        )}
-                    </ul>
+                                            >
+                                                <img src={menu.src} className="w-4 h-4" alt="" />
+                                                <span className={`${!open && "hidden"} origin-left duration-200 text-base`}>
+                                                    {menu.title}
+                                                </span>
+                                            </li>
+                                        </div>
+                                    </NavLink>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                    <div className={!open ? "invisible sm:visible content-bottom" : "visible content-bottom"}>
+                        <ul className={`flex flex-col items-center mb-3 text-gray-300 transition-all text-sm ${!open && "scale-0"}`}>
+                            <p >Logged as <span className="font-semibold text-orange">{user.username ? user.username : 'anonim'}</span></p>
+                            <p className="font-semibold text-orange">{user.email}</p>
+                        </ul>
+                        <ul>
+                            {user && (
+                                <div className="logout">
+                                    {/* <span>{user.email}</span> */}
+                                    {/* <button onClick={handleClick} className={`text-white bg-red-500 hover:bg-red-700 focus:scale-90 focus:transition-all rounded-lg px-5 py-2.5 text-center mb-2 w-full transition-all ${!open && "scale-0"}`}>Log out</button> */}
+                                </div>
+                            )}
+                        </ul>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
 
--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -5,13 +5,11 @@ const NotFound = () => {
         <div className="py-16 px-2 sm:px-16">
             <div className="flex justify-center items-center mt-24 lg:mt-4">
                 <div className="py-8 pl-8 sm:pl-0 sm:py-0 w-1/2">
-                    <h1 className="text-5xl sm:text-6xl font-bold text-orange">404</h1>
+                    <h1 className="text-5xl sm:text-8xl font-bold text-orange">404</h1>
                     <p className="text-sm mb-8 sm:mb-0 sm:text-lg pb-4 pt-2 sm:pt-1 sm:pb-10">Sorry, you've found a page that doesnt exist 😞</p>
                 </div>
-                <div>
-                    <div className="flex justify-end w-1/2 ml-8">
-                        <img src={notFound} className="hidden lg:block" alt="Task PNG" />
-                    </div>
+                <div className="flex justify-end w-1/2">
+                    <img src={notFound} className="hidden lg:block" alt="Task PNG" />
                 </div>
             </div>
         </div>
